5466db5540c4c7295f63f440ed1b52b4362ebf4b
Hai Truong <haitruong1405@gmail.com>
2021-09-07 18:01:25 +0700

    update endpoint host

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -2,16 +2,15 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
-import TextContainer from "../TextContainer/TextContainer";
 import Messages from "../Messages/Messages";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
 
 import "./Chat.css";
 
-// const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+const ENDPOINT = "https://vouch-chat-application.herokuapp.com/";
 
-const ENDPOINT = "localhost:5000";
+// const ENDPOINT = "localhost:5000";
 
 let socket;
 
